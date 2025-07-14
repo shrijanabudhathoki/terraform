70331d87e023af572ed7362b62b3fe570e658104
shrijana <shrijana@192.168.77.138>
2025-07-14 03:33:17 -0400

    edit

--- ec2-project/main.tf
+++ ec2-project/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.3.0"
+    }
+  }
+}
+
 provider "aws" {
     region = "us-east-1"
 }
